915838e9c0360831c0eed913ca5d590ceba96857
Кривов Сергей Сергеевич (4064688) <sskrivov@vtb.ru>
2022-10-19 12:40:12 +0300

    Test comment

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler, ConversationHandler, MessageHandler, \
     filters, CallbackQueryHandler, Job, JobQueue
 
-# Добавил коммент1111
+# Добавил коммент2222
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
